2c98799dd9dc7f5dbdf49f78eb1391ad79173448
Gautam Kumar <kumargautam@vmware.com>
2018-08-10 11:21:42 +0530

    controller changes
    Change-Id: I8c7831f2dbc154d73832c0cce8fbe9dcf4c5614a

--- boot-mongodb-producer/src/main/java/com/vmware/boot/producer/controller/StudentProducerController.java
+++ boot-mongodb-producer/src/main/java/com/vmware/boot/producer/controller/StudentProducerController.java
@@ -20,7 +20,7 @@ import io.swagger.annotations.Api;
 @RequestMapping(value = "/api")
 @Api(value="/api")
 public class StudentProducerController {
-	private final Logger logger = LoggerFactory.getLogger(getClass());
+	private static final Logger LOG = LoggerFactory.getLogger(StudentProducerController.class);
 	private final StudentDao studentRepository;
 
 	public StudentProducerController(StudentDao studentRepository) {
@@ -29,22 +29,22 @@ public class StudentProducerController {
 
 	@RequestMapping(method = RequestMethod.POST, value = "/student/add",produces= MediaType.APPLICATION_JSON_VALUE)
 	public Student addStudent(@RequestBody Student student) {
-		logger.info("Start addStudent() method in StudentControllerWithMongoTemplate.class");
-		logger.info("End addStudent() method in StudentControllerWithMongoTemplate.class");
+		LOG.info("Start addStudent() method in StudentControllerWithMongoTemplate.class");
+		LOG.info("End addStudent() method in StudentControllerWithMongoTemplate.class");
 		return studentRepository.addStudent(student);
 	}
 
 	@RequestMapping(method = RequestMethod.PUT, value = "/student/{studentId}",produces= MediaType.APPLICATION_JSON_VALUE)
 	public Student updateStudent(@PathVariable int studentId, @RequestBody Student student) {
-		logger.info("Start updateStudent() method in StudentControllerWithMongoTemplate.class");
-		logger.info("End updateStudent() method in StudentControllerWithMongoTemplate.class");
+		LOG.info("Start updateStudent() method in StudentControllerWithMongoTemplate.class");
+		LOG.info("End updateStudent() method in StudentControllerWithMongoTemplate.class");
 		return studentRepository.updateStudent(studentId, student);
 	}
 
 	@RequestMapping(method = RequestMethod.DELETE, value = "/student/{studentId}",produces= MediaType.APPLICATION_JSON_VALUE)
 	public List<Student> deleteStudent(@PathVariable int studentId) {
-		logger.info("Start deleteStudent() method in StudentControllerWithMongoTemplate.class");
-		logger.info("End deleteStudent() method in StudentControllerWithMongoTemplate.class");
+		LOG.info("Start deleteStudent() method in StudentControllerWithMongoTemplate.class");
+		LOG.info("End deleteStudent() method in StudentControllerWithMongoTemplate.class");
 		return studentRepository.deleteStudent(studentId);
 	}
 }
